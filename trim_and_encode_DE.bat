6de8ea6a5a17f0e7006b05997e1aecf80974a4f1
Janis Mades <15909944+nurjns@users.noreply.github.com>
2026-09-01 13:32:18 +0200

    Update trim_and_encode_DE.bat

--- trim_and_encode_DE.bat
+++ trim_and_encode_DE.bat
@@ -1,4 +1,4 @@
-:: Version 2.4.4 - 01.09.2026 - @nurjns
+:: Version 2.4.5 - 01.09.2026 - @nurjns
 
 @echo off
 setlocal enabledelayedexpansion
@@ -63,7 +63,7 @@ set CRF_WERT=
 if "%CODECWAHL%"=="1" ( 
 	echo H.265 CRF-Wert ^(18=hoch, 24=normal, 30=niedrig, 35=sehr niedrig^) - Leer lassen = nur schneiden 
 ) else (
-	echo AV1 CRF-Wert ^(20=sehr hoch, 24=hoch, 30=normal, 40=niedrig, 50=sehr niedrig^) - Leer lassen = nur schneiden 
+	echo AV1 CRF-Wert ^(20=sehr hoch, 26=hoch, 35=normal, 45=niedrig, 55=sehr niedrig^) - Leer lassen = nur schneiden 
 ) 
 set /p CRF_WERT="Welcher CRF-Wert soll verwendet werden? "
  
@@ -71,7 +71,7 @@ if not "%CRF_WERT%"=="" (
 	echo %CRF_WERT%| findstr /r "^[0-9][0-9]*$" >nul
 	if errorlevel 1 goto :CRF_FEHLER
 	if "%CRF_WERT%" LSS "16" goto :CRF_FEHLER
-	if "%CRF_WERT%" GTR "50" goto :CRF_FEHLER
+	if "%CRF_WERT%" GTR "55" goto :CRF_FEHLER
 )
 goto :CRF_OK
 :CRF_FEHLER
